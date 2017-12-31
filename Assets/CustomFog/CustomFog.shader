55087fdd3502660ffc0b389dd94d95fb2f8be413
Leonardo <leomontes@outlook.fr>
2017-12-31 17:58:28 +0100

    Final commit

---
+++ Assets/CustomFog/CustomFog.shader
@@ -0,0 +1,80 @@
+﻿Shader "Hidden/Custom fog" {
+	Properties {
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader {
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f {
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 worldDirection : TEXCOORD1;
+			};
+
+			sampler2D _CameraDepthTexture;
+			float4x4 clipToWorld;
+
+			v2f vert (appdata v) {
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+
+				UNITY_TRANSFER_DEPTH(o.depth);
+				
+				float4 cli = float4(o.vertex.xy, 0.0, 1.0);
+				o.worldDirection = mul(clipToWorld, cli) - _WorldSpaceCameraPos;
+
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			uniform half4 _Fog0Color;
+			uniform half4 _Fog1Color;
+			uniform half _HeightPower;
+			uniform half _HeightOffset;
+
+			uniform half _BlurDepthPow;
+
+			sampler2D _BlurDepth;
+
+			sampler2D _Falloff;
+
+			fixed4 frag (v2f i) : SV_Target {
+				float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv.xy);
+				float depthEye = LinearEyeDepth(depth);	
+				float falloff = 1 - tex2D(_Falloff, float2(Linear01Depth(depth), 0)).r;
+
+				float3 worldspace = i.worldDirection * depthEye + _WorldSpaceCameraPos;
+
+				float4 blurDepth = tex2D(_BlurDepth, i.uv);
+
+
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				half4 gradient = lerp(_Fog0Color, _Fog1Color, saturate((worldspace.y - _HeightOffset) / _HeightPower));
+				col = lerp(col, gradient, falloff);
+				col += blurDepth * _BlurDepthPow;
+
+				//col = depth;
+				//col = falloff;
+				//col = blurDepth;
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/CustomFog/CustomFogImageEffect.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomFogImageEffect : MonoBehaviour {
+
+    // Import
+    [Header("Import")]
+    [SerializeField] private Shader shaderFog;
+    [SerializeField] private Shader shaderBlur;
+    [SerializeField] private Shader shaderDepthBlur;
+    private Material material0;
+    private Material material1;
+    private Material material2;
+    private new Camera camera;
+
+    // Fog
+    [Header("Fog")]
+    [SerializeField] private Color fog0Color;
+    [SerializeField] private Color fog1Color;
+    [SerializeField] private float heightPower = 100;
+    [SerializeField] private float heightOffset = -50;
+    [SerializeField][Range(0.0f, 1.0f)] private float power = 1;
+
+    // Fog falloff
+    [Header("Fog falloff")]
+    [SerializeField] private int resolution = 512;
+    [SerializeField] private AnimationCurve falloff;
+
+    // Blur
+    [Header("Blur")]
+    [SerializeField] private int blurDownRes;
+    [SerializeField] private int blurIterations;
+    [SerializeField] private Color blurColor;
+
+    // Blur falloff
+    [Header("Blur falloff")]
+    [SerializeField]
+    private int blurFalloffResolution;
+    [SerializeField] private AnimationCurve blurFalloff;
+
+
+
+    private void OnEnable() {
+        // Setup
+        material0 = new Material(shaderFog);
+        material1 = new Material(shaderDepthBlur);
+        material2 = new Material(shaderBlur);
+        camera = GetComponent<Camera>();
+        camera.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        // Falloff
+        Texture2D blurFalloffTexture = new Texture2D(blurFalloffResolution, 1);
+        for (int x = 0; x < blurFalloffResolution; x++) {
+            blurFalloffTexture.SetPixel(x, 0, Color.white * (1f - blurFalloff.Evaluate((float)x / blurFalloffResolution)));
+        }
+        blurFalloffTexture.Apply();
+        blurFalloffTexture.wrapMode = TextureWrapMode.Clamp;
+
+        Texture2D falloffTexture = new Texture2D(resolution, 1);
+        for (int x = 0; x < resolution; x++) {
+            falloffTexture.SetPixel(x, 0, Color.white * (1f - falloff.Evaluate((float)x / resolution)));
+        }
+        falloffTexture.Apply();
+        falloffTexture.wrapMode = TextureWrapMode.Clamp;
+
+
+        // Blur
+        int width = src.width >> blurDownRes;
+        int height = src.height >> blurDownRes;
+
+        material1.SetColor("_Tint", blurColor);
+        material1.SetTexture("_Falloff", blurFalloffTexture);
+
+        RenderTexture rt = RenderTexture.GetTemporary(width, height);
+        Graphics.Blit(src, rt);
+
+        for (int i = 0; i < blurIterations; i++) {
+            RenderTexture rt2 = RenderTexture.GetTemporary(width, height);
+            Graphics.Blit(rt, rt2, i == 0 ? material1 : material2);
+            RenderTexture.ReleaseTemporary(rt);
+            rt = rt2;
+        }
+
+
+        // Fog
+        material0.SetColor("_Fog0Color", fog0Color);
+        material0.SetColor("_Fog1Color", fog1Color);
+        material0.SetFloat("_HeightPower", heightPower);
+        material0.SetFloat("_HeightOffset", heightOffset);
+        material0.SetFloat("_BlurDepthPow", power);
+        material0.SetTexture("_Falloff", falloffTexture);
+
+        Matrix4x4 p = GL.GetGPUProjectionMatrix(camera.projectionMatrix, false);
+        p[2, 3] = p[3, 2] = 0.0f;
+        p[3, 3] = 1.0f;
+        Matrix4x4 clipToWorld = Matrix4x4.Inverse(p * camera.worldToCameraMatrix) * Matrix4x4.TRS(new Vector3(0, 0, -p[2, 2]), Quaternion.identity, Vector3.one);
+        material0.SetMatrix("clipToWorld", clipToWorld);
+
+        // Done
+        material0.SetTexture("_BlurDepth", rt);
+        Graphics.Blit(src, dst, material0);
+        RenderTexture.ReleaseTemporary(rt);
+    }
+}
